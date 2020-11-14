10e7daff4cafc794f7c4a11c0754376d9d20177d
notWilll <74434050+notWilll@users.noreply.github.com>
2020-11-13 19:27:19 -0500

    Update main.c

--- main.c
+++ main.c
@@ -1,9 +1,11 @@
 /*
- -------------------------------------
+ --------------------------------------------------------------------------------
  File:    main.c
- Caesar's Cipher
+ Description: Caesar's Cipher is a type of substitution cipher in which each 
+ letter in the plaintext is replaced by a letter some fixed number of positions 
+ down the alphabet
  Author:  William Mabia
- -------------------------------------
+ --------------------------------------------------------------------------------
  */
 
 #include <stdio.h>
@@ -11,45 +13,54 @@
 #include <string.h>
 
 //Encoder
-
-int main() {
+void encode(char file_name[], int key) {
 	char message[1000];
-	int i, key = 0, length, l = 10, j;
-
-	//take input from character
-	printf("Enter characters to encrypt: \n");
-	gets(message);
-
-	//Take input key
-	printf("Enter key: ");
-	scanf("%d", &key);
+	int i, length;
+	FILE *outfile;
+	
+	//Open the file and get the get its contents.
+	outfile = fopen(file_name, "r");
+	fgets(message, 50, outfile);
+	
+	//Print the contents of the file.
+	printf("Message: %s\n", message);
 
 	length = strlen(message);
-
-	if (length == 0) { // if statement that checks if input is empty
+	
+	//Ensure file is not empty
+	if (length == 0) {
 		printf("No message provided");
 	}
 
 	else if (length > 0) {
-		for (i = 0; message[i]; i++) { // for loops goes through all characters in the message array (i didn't use i<length because message[i] goes through message array until it's empty. rember i can also use message[i] != '\0' cause the end of array has '\0'
+		//for loop goes through all characters in the message array (i didn't use "i<length" because message[i] goes through message array until final char
+		for (i = 0; message[i]; i++) {  
 
-			// for capital letters
+			// for upper case letters
 			if (message[i] >= 'A' && message[i] <= 'Z') {
 				message[i] -= key;
-				if (message[i] > 'Z') { // if key makes a character go over z
+				if (message[i] > 'Z') { // if key makes a character go over "Z"
 					message[i] -= 26; // it uses the ascii value to make values over z go back and start at a
 				}
 			}
-			// this is the earlier if statement for lower case characters
+			//for lower case characters
 			else if (message[i] >= 'a' && message[i] <= 'z') {
 				message[i] += key;
+				//This if statement ensure whenever the key makes the letter go over 
 				if (message[i] > 'z') {
 					message[i] -= 26;
 				}
 			}
 		}
 	}
-
-	printf("%s\n", message);
+	printf("\n");
+	printf("Encryption: %s\n\n", message);
 
 }
+int main() {
+	
+	//encode("textforencoding.txt", 5);
+	//decode("textfordecoding.txt", 5);
+
+}
+
